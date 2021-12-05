965751accbdc21c4525f7f81873ebdf884c6739b
Heather L <92494436+hal1277@users.noreply.github.com>
2021-12-05 12:15:56 -0500

    adding retirment csv

--- Schema.sql
+++ Schema.sql
@@ -44,7 +44,7 @@ CREATE TABLE dept_emp (
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	PRIMARY KEY (emp_no)
+	PRIMARY KEY (emp_no, dept_no)
 );
 
 CREATE TABLE titles (
@@ -52,7 +52,7 @@ CREATE TABLE titles (
 	title VARCHAR(40) NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	PRIMARY KEY (emp_no)
+	PRIMARY KEY (emp_no, title)
 );	
 
 SELECT * FROM departments;
